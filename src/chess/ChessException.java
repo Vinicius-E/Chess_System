26e56a34de477daf66ee77a1416fb9119aba39ee
Vinicius-E <viniciuseduardo0702@hotmail.com>
2021-11-08 14:49:11 -0300

    Version 1.0

---
+++ src/chess/ChessException.java
@@ -0,0 +1,12 @@
+package chess;
+
+import boardGame.BoardException;
+
+public class ChessException extends BoardException{
+
+	private static final long serialVersionUID = 1L;
+
+	public ChessException(String msg) {
+		super(msg);
+	}
+}
